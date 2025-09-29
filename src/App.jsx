d3070769fa914b55463298dc541f295f95c6a464
Sekaii22 <shawn.ng22@gmail.com>
2025-09-30 04:38:36 +0800

    Update body, #root, and main styling to fit in a header

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,17 @@ import ExperienceSection from './components/ExperienceSection';
 function App() {
   return (
     <>
-      {/* <InfoSection></InfoSection> */}
-      {/* <EducationSection></EducationSection> */}
-      <ExperienceSection></ExperienceSection>
+      <header>
+        <h1>CV Builder</h1>
+        <p>A React assignment for The Odin Project.</p>
+      </header>
+      <main>
+        {/* <InfoSection></InfoSection> */}
+        {/* <EducationSection></EducationSection> */}
+        <ExperienceSection></ExperienceSection>
+      </main>
     </>
   )
 }
 
 export default App
-
-/*
-<div>
-  <a href="https://vite.dev" target="_blank">
-    <img src={viteLogo} className="logo" alt="Vite logo" />
-  </a>
-  <a href="https://react.dev" target="_blank">
-    <img src={reactLogo} className="logo react" alt="React logo" />
-  </a>
-</div>
-<h1>Vite + React</h1>
-<div className="card">
-  <button onClick={() => setCount((count) => count + 1)}>
-    count is {count}
-  </button>
-  <p>
-    Edit <code>src/App.jsx</code> and save to test HMR
-  </p>
-</div>
-<p className="read-the-docs">
-  Click on the Vite and React logos to learn more
-</p>
-*/
